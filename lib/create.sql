683accc2d00946082a3ff2197f6bebb52e8fa70e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-20 04:40:08 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
