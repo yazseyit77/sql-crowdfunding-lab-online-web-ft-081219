8f9ed7abbd7f518625bbb6cf983eeaf58a70ab9a
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-20 05:21:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,8 +29,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   LEFT JOIN pledges
   ON pledges.project_id = projects.id
-  GROUP BY projects.title
-  ORDER BY projects.title;"
+  GROUP BY pledges.amount
+  HAVING SUM(pledges.amount) = projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
