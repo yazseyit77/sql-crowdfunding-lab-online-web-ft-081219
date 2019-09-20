8c167d064fbb954d8fb9d29af2f3974c596e3b90
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-20 05:06:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, pledges.amount
   FROM projects
-  LEFT JOIN pledges
+  JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY pledges.amount
   ORDER BY title"
