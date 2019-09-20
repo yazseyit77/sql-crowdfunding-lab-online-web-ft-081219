b5468696b21928fb18a754c049f68ba2f46673e8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-20 05:23:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, projects.funding_goal, SUM(pledges.amount)
+  "SELECT projects.title, projects.funding_goal
   FROM projects
   LEFT JOIN pledges
   ON pledges.project_id = projects.id
