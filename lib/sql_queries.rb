71154e54065367b712f1af1371aa8f80ea8b8238
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-20 05:15:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT users.name, users.age, SUM(pledges.amount)
+  FROM users
+  LEFT JOIN pledges
+  ON pledges.user_id = users.id
+  GROUP BY users.name
+  ORDER BY users.name;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
